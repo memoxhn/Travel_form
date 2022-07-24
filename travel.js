e4a001d75a7a4856d9d1f9cc87c10363fba4b818
Melvin Rosales <melvinre1997@gmail.com>
2022-07-23 23:37:25 -0600

    changed travel info to snake case

--- travel.js
+++ travel.js
@@ -1,26 +1,31 @@
 const submitForm = (event) => {
-    event.preventDefault();
-    const name = document.getElementById("name").value
-    const email = document.getElementById("email").value
-    const selectpackage = document.getElementById("identifier-for-select_package").value
-    const date = document.getElementById("date").value
-    const persons = document.getElementById("identifier-for-persons").value
-    const discount = document.getElementById("discount").value
+  event.preventDefault();
 
-    if (!name || !email || !selectpackage || !date || !persons) {
-        alert("porfavor llena todos los campos")
-        return;
-    }
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const selectpackage = document.getElementById(
+    "identifier-for-select_package"
+  ).value;
+  const date = document.getElementById("date").value;
+  const persons = document.getElementById("identifier-for-persons").value;
+  const discount = document.getElementById("discount").value;
 
-    const travelinfo = {
-        userName: name,
-        userEmail: email,
-        userSelectPackage: selectpackage,
-        userDate: date,
-        userPersons: persons,
-        userDiscount: discount,
-    }
-    console.log(travelinfo)
+  if (!name || !email || !selectpackage || !date || !persons) {
+    alert("porfavor llena todos los campos");
+    return;
+  }
 
+  // userName - Camel case
+  // user_name - Snake case
 
-}
+  const travelInfo = {
+    user_name: name,
+    user_email: email,
+    user_selected_package: selectpackage,
+    user_date: date,
+    user_persons: persons,
+    user_discount: discount,
+  };
+
+  console.log(travelInfo);
+};
